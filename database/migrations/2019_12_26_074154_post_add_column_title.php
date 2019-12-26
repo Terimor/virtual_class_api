bf56c8eaf7286d2ac61bc39d2250983f9b49a5d9
Vlad Kyrychenko <vlad.kyr199@gmail.com>
2019-12-26 10:15:08 +0200

    added title adding migration

---
+++ database/migrations/2019_12_26_074154_post_add_column_title.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PostAddColumnTitle extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
