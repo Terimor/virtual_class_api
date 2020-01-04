6861ff586e6a9744cf36f6ea8c1250348703a61b
Vlad Kyrychenko <vlad.kyr199@gmail.com>
2020-01-04 16:06:55 +0200

    fixed middleware

--- app/Http/Middleware/CheckClassMember.php
+++ app/Http/Middleware/CheckClassMember.php
@@ -16,8 +16,7 @@ class CheckClassMember extends PermissionBase
      */
     public function handle($request, Closure $next)
     {
-        dd($this->user->id, $request->route('class', 0)->id);
-        if(!Member::count($this->user->id, @$request->route('class', 0)->id)) {
+        if(!Member::count($this->user->id, @$request->route('class')->id)) {
             return response()->json(self::ACCESS_FORBIDDEN, 403);
         }
         return $next($request);
