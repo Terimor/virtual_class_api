8034504f1d3b97bef715b0e38159922f5ad2b74c
Vlad Kyrychenko <vlad.kyr199@gmail.com>
2020-01-08 02:29:11 +0200

    added notifications amount

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -8,8 +8,12 @@ use Illuminate\Http\Request;
 
 class NotificationsController extends Controller
 {
-    public function index(StudyingClass $class) {
-        return Notification::orderBy('id', 'DESC')->get();
+    public function index(StudyingClass $class, Request $request) {
+        $query = Notification::orderBy('id', 'DESC');
+        if($amount = $request->get('amount', false)) {
+            $query->limit($amount);
+        }
+        return $query->get();
     }
 
     public function show(StudyingClass $class, Notification $notification) {
